8f4adbf0f0bb83cdcc32608f3dc7f66f0af2d8d8
suchismitarout <suchismitarout47@gmail.com>
2019-09-26 17:39:45 +0530

    data

---
+++ student_data.py
@@ -0,0 +1,4 @@
+{"name": "surya", "age": 28, "id": 11234, "salary": 56000, "loc": "blr"}
+{"name": "suraj", "age": 25, "id": 12134, "salary": 34000, "loc": "kolkata"}
+{"name": "khirod", "age": 27, "id": 16753, "salary": 42000, "loc": "delhi"}
+{"name": "piyush", "age": 29, "id": 12345, "salary": 35000, "loc": "pune"}
