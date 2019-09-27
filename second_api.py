3ba618c438eb446ec79ae72418a56279f2419582
suchismitarout <suchismitarout47@gmail.com>
2019-09-27 16:59:56 +0530

    after changes

--- second_api.py
+++ second_api.py
@@ -1,16 +1,20 @@
 from flask import Flask, request,jsonify
-app=Flask(__name__)
 import json
+
+app = Flask(__name__)
+
 with open("C:/Users/Ranjitha/PycharmProject/rest_api_prj/student_data.py", "r") as fp:
     content = fp.read()
     d = content.split("\n")
     # print(d)
-    res=[json.loads(i) for i in d]
+    res = [json.loads(i) for i in d if len(i) > 0]
     # print(res)
+
 
 @app.route('/', methods=['GET'])
 def get_student_info():
     return jsonify(res)
+
 
 @app.route('/getstudentbyid/<int:id>', methods=['GET'])
 def get_student_info_by_id(id):
@@ -20,12 +24,14 @@ def get_student_info_by_id(id):
             data = i
     return jsonify(data)
 
+
 @app.route('/deletebyid/<int:id>', methods=['DELETE'])
 def delete_by_student_by_id(id):
     for i in res:
         if i["id"] == id:
             res.remove(i)
     return jsonify(res)
+
 
 @app.route('/add_new_student', methods=['POST'])
 def add_new_student():
@@ -37,11 +43,17 @@ def add_new_student():
     d = {"name": name, "age": age, "id": id, "salary": salary, "loc": loc}
     data = request.get_json(d)
     res.append(data)
+    with open ("C:/Users/Ranjitha/PycharmProject/rest_api_prj/student_data.py", "w") as fw:
+        for i in res:
+            fw.write(json.dumps(i))
+            fw.write("\n")
+
     return jsonify(res)
 
 
 if __name__ =='__main__':
     app.run()
+
 
 
 
